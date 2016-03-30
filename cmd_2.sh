0f3e01d47e9a99a707502361dea122ee0e6d71ef
Zhicai <zzhang2@caltech.edu>
2016-03-30 07:45:45 +0200

    commands to do regression

---
+++ cmd_2.sh
@@ -0,0 +1,4 @@
+root -l eregtrainingExample_13TeV_Pi0_gamma2.C+(false,false)
+root -l eregtrainingExample_13TeV_Pi0_gamma2.C+(true,false)
+root -l eregtestingExample_13TeV_Pi0_gamma2.C+(false,false)
+root -l eregtestingExample_13TeV_Pi0_gamma2.C+(true,false)
