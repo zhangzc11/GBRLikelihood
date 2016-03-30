932c717abe8128505dda9076a7190db7f8ec2d97
Zhicai Zhang <zzhang2@caltech.edu>
2016-03-30 15:28:38 -0700

    Delete cmd_2.sh

--- cmd_2.sh
+++
@@ -1,4 +0,0 @@
-root -l eregtrainingExample_13TeV_Pi0_gamma2.C+(false,false)
-root -l eregtrainingExample_13TeV_Pi0_gamma2.C+(true,false)
-root -l eregtestingExample_13TeV_Pi0_gamma2.C+(false,false)
-root -l eregtestingExample_13TeV_Pi0_gamma2.C+(true,false)
