1f33a5f9010a35d3e0c92a86acf203897af9110b
Zhicai Zhang <zzhang2@caltech.edu>
2016-03-31 11:45:19 -0700

    Delete gamma2_EB.sh

--- scripts/submit/gamma2_EB.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /afs/cern.ch/work/z/zhicaiz/private/regression/CMSSW_7_4_12/src/HiggsAnalysis/GBRLikelihood/
-ulimit -c 0
-eval `scramv1 runtime -sh`
-date 
-root -q -l -b eregtraining_13TeV_Pi0.C+\(true,false,true\) 
-root -q -l -b eregtesting_13TeV_Pi0.C+\(true,false,true\) 
-date 
