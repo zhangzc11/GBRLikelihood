17df09a7bdb409a5562060b3b6a92249c43872d1
Zhicai <zzhang2@caltech.edu>
2016-04-03 01:32:33 +0200

    updating...

--- cmd_test_bothGamma.sh
+++ cmd_test_bothGamma.sh
@@ -1,7 +1,5 @@
 date
 mkdir -p log
-root -q -l -b eregtraining_13TeV_Pi0_bothGamma.C+(false,false) > log/reg_log_EE.log
-root -q -l -b eregtraining_13TeV_Pi0_bothGamma.C+(true,false) > log/reg_log_EB.log 
 root -q -l -b eregtesting_13TeV_Pi0_bothGamma.C+(false,false) > log/test_log_EE.log
 root -q -l -b eregtesting_13TeV_Pi0_bothGamma.C+(true,false) > log/test_log_EB.log
 date
