62b1d9029255b06c3d1a35adc3016173d6c8572f
Zhicai <zzhang2@caltech.edu>
2016-04-14 11:12:11 +0200

    updating

--- scripts/cmd.sh
+++ scripts/cmd.sh
@@ -1,6 +1,2 @@
 python submit_pi0.py "EE" "0"
 python submit_pi0.py "EB" "0"
-python submit_pi0.py "EE" "1"
-python submit_pi0.py "EB" "1"
-python submit_pi0.py "EE" "2"
-python submit_pi0.py "EB" "2"
